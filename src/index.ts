6a742b412ae2b7b1f9f252b9e5ee6509ece9ec9b
Dhrumil-sim <dhrumil.panchal@simformsolutions.com>
2025-07-18 14:23:50 +0530

    fix: log

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ connectDB()
   .then(() => {
     app.listen(PORT, () => {
       console.log(`⚙️ Server is running at: http://localhost:${PORT}`);
+      console.log('demo');
     });
   })
   .catch((err) => {
