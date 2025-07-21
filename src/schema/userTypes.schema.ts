09ebc9df6c883da3423c21d4e39fc3be6edb276f
Dhrumil-sim <dhrumil.panchal@simformsolutions.com>
2025-07-21 14:20:59 +0530

    fix: modified primary key id

--- src/schema/userTypes.schema.ts
+++ src/schema/userTypes.schema.ts
@@ -1,8 +1,8 @@
-import { mysqlTable, timestamp, varchar } from 'drizzle-orm/mysql-core';
+import { int, mysqlTable, timestamp, varchar } from 'drizzle-orm/mysql-core';
 import { timestamps } from './helpers/column.helpers';
 
 export const userTypes = mysqlTable('user_types', {
-  userTypeId: varchar('user_type_id', { length: 36 }).primaryKey(),
+  userTypeId: int('user_type_id').autoincrement().primaryKey(),
   typeName: varchar('type_name', { length: 100 }).unique(), // e.g., 'Client', 'Vendor'
   description: varchar('description', { length: 255 }),
   createdAt: timestamp('created_at', { mode: 'date' }),
