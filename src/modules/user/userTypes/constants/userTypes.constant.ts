927a60b0506a290b476f3e07b377114174db4009
Dhrumil-sim <dhrumil.panchal@simformsolutions.com>
2025-07-22 13:48:52 +0530

    feat: added constants required for the creating User Types

---
+++ src/modules/user/userTypes/constants/userTypes.constant.ts
@@ -0,0 +1,10 @@
+export enum UserTypeErrorCode {
+  CONFLICT = 'USER_TYPE_ALREADY_EXISTS',
+}
+export enum UserTypeAction {
+  CREATE_USER_TYPE = 'Creating user type !',
+}
+export enum UserTypeMessage {
+  CONFLICT = 'User type is already exists with the given name !',
+  CREATE_USER_TYPE = 'User Type Created Succesfully ! ',
+}
