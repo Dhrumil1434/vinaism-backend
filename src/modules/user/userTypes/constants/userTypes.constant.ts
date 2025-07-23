822cc3b3b556c23dab35669347a301439f1ab739
Dhrumil-sim <dhrumil.panchal@simformsolutions.com>
2025-07-23 17:01:16 +0530

    feat: added required message constants

--- src/modules/user/userTypes/constants/userTypes.constant.ts
+++ src/modules/user/userTypes/constants/userTypes.constant.ts
@@ -1,11 +1,16 @@
 export enum UserTypeErrorCode {
   CONFLICT = 'USER_TYPE_ALREADY_EXISTS',
+  BAD_REQUEST = 'USER_TYPE_BAD_REQUEST',
+  NOT_FOUND = 'USER_TYPE_NOT_FOUND',
 }
 export enum UserTypeAction {
   CREATE_USER_TYPE = 'Creating user type !',
+  UPDATE_USER_TYPE = 'Updating user type !',
 }
 export enum UserTypeMessage {
   CONFLICT = 'User type is already exists with the given name !',
   CREATE_USER_TYPE = 'User Type Created Succesfully ! ',
   GET_USER_TYPE = 'User Types fetched successfully !',
+  UPDATE_USER_TYPE_SUCCESS = 'User Type Updated Successfully !',
+  UPDATE_USER_TYPE_FAILED = 'User Type Is Not Updated !',
 }
