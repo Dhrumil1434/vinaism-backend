adef26d01ef7aa05ac01f0986de80546e29e3761
Dhrumil-sim <dhrumil.panchal@simformsolutions.com>
2025-07-24 16:04:10 +0530

    feat: modified email field

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -7,7 +7,7 @@ export const users = mysqlTable('users', {
   userName: varchar('user_name', { length: 255 }).notNull(),
   profilePicture: varchar('profile_picture', { length: 200 }).notNull(),
   phoneNumber: varchar('phone_number', { length: 20 }).notNull().unique(),
-  email: varchar('email', { length: 255 }).unique(),
+  email: varchar('email', { length: 255 }).notNull().unique(),
   firstName: varchar('first_name', { length: 150 }).notNull(),
   lastName: varchar('last_name', { length: 150 }).notNull(),
   password: varchar('password', { length: 255 }),
