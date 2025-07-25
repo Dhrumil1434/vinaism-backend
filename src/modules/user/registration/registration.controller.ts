60975da9a89438f06f491ff9e4131968ff1332f9
Dhrumil-sim <dhrumil.panchal@simformsolutions.com>
2025-07-25 17:03:41 +0530

    feat: used the service function to the controller with the proper pre validation along with the response structure

---
+++ src/modules/user/registration/registration.controller.ts
@@ -0,0 +1,54 @@
+import { ApiResponse, asyncHandler } from '@utils-core';
+import { NextFunction, Request, Response } from 'express';
+import z from 'zod';
+import {
+  UserRegistrationInsertSchemaDto,
+  UserRegistrationResponseSchema,
+} from './validators/registration.dtos';
+
+import path from 'path';
+import { configDotenv } from 'dotenv';
+import {
+  generateFileName,
+  saveBufferedFile,
+} from 'utils/saveBufferedFile.util';
+import {
+  validateUniqueEmail,
+  validateUniquePhoneNumber,
+  validateUserTypeExists,
+} from './validators/registration.validators';
+import { UserRegistrationService } from './registration.service';
+import { StatusCodes } from 'http-status-codes';
+import { UserRegistrationMessage } from './registration.constants';
+configDotenv();
+
+export class UserRegistrationController {
+  static registerUser = asyncHandler(
+    async (req: Request, res: Response, _next: NextFunction) => {
+      const validateInsert = z.parse(UserRegistrationInsertSchemaDto, req.body);
+      const uploadDir = process.env['UPLOAD_DIRECTORY'] || 'public/uploads';
+
+      // Only handle file if present
+      if (req.file && req.file.buffer) {
+        const fileName = generateFileName(req.file.originalname);
+        const filePath = path.join(uploadDir, fileName);
+        await saveBufferedFile(req.file.buffer, filePath);
+      }
+      await validateUserTypeExists(validateInsert.userType);
+      await validateUniqueEmail(validateInsert.email);
+      await validateUniquePhoneNumber(validateInsert.phoneNumber);
+      const insertedRecord =
+        await UserRegistrationService.registerUser(validateInsert);
+      const validateResponse = z.parse(
+        UserRegistrationResponseSchema,
+        insertedRecord
+      );
+      const response = new ApiResponse(
+        StatusCodes.CREATED,
+        validateResponse,
+        UserRegistrationMessage.REGISTERED_SUCCESSFULLY
+      );
+      res.status(response.statusCode).json(response);
+    }
+  );
+}
