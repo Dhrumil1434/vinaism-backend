d1b565a69bdc85e2f8d584c41c56372ef5451dd3
Dhrumil-sim <dhrumil.panchal@simformsolutions.com>
2025-07-26 16:54:14 +0530

    feat: exported the required type

--- src/modules/user/registration/registration.types.ts
+++ src/modules/user/registration/registration.types.ts
@@ -1,8 +1,14 @@
 import z from 'zod';
-import { UserRegistrationInsertSchemaDto } from './validators/registration.dtos';
+import {
+  userRegistrationFilterSchema,
+  UserRegistrationInsertSchemaDto,
+} from './validators/registration.dtos';
 import { userTypeIdParamSchema } from '../userTypes/validators/userType.validator';
 
 export type IUserRegistrationInsert = z.infer<
   typeof UserRegistrationInsertSchemaDto
 >;
 export type IUserTypeId = z.infer<typeof userTypeIdParamSchema>;
+export type UserRegistrationFilters = z.infer<
+  typeof userRegistrationFilterSchema
+>;
