f7aed5df2248e3b16a1c6e4042bb597d04b38dab
Dhrumil-sim <dhrumil.panchal@simformsolutions.com>
2025-07-26 17:02:36 +0530

    feat: modified the user schema based on the requirement

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -1,4 +1,10 @@
-import { int, mysqlTable, varchar } from 'drizzle-orm/mysql-core';
+import {
+  int,
+  mysqlTable,
+  varchar,
+  boolean,
+  timestamp,
+} from 'drizzle-orm/mysql-core';
 import { userTypes } from './userTypes.schema';
 import { timestamps } from './helpers/column.helpers';
 
@@ -12,5 +18,10 @@ export const users = mysqlTable('users', {
   lastName: varchar('last_name', { length: 150 }).notNull(),
   password: varchar('password', { length: 255 }),
   userType: int('user_type_id').references(() => userTypes.userTypeId),
+  email_verified: boolean('email_verified').default(false).notNull(),
+  phone_verified: boolean('phone_verified').default(false).notNull(),
+  otp_code: varchar('otp_code', { length: 6 }),
+  otp_expires_at: timestamp('otp_expires_at'),
+  admin_approved: boolean('admin_approved').default(false).notNull(),
   ...timestamps,
 });
